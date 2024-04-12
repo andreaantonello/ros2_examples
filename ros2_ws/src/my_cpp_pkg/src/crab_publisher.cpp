140e0726cc0f9e23e5b1b014c722bc6cdef2c03f
andreaantonello <andrea.antonello.it@gmail.com>
2024-04-12 15:38:10 +0100

    added crab publisher

--- ros2_ws/src/my_cpp_pkg/src/crab_publisher.cpp
+++ ros2_ws/src/my_cpp_pkg/src/crab_publisher.cpp
@@ -1,23 +1,40 @@
 #include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/msg/float32_multi_array.hpp"
+#include "custom_crab_interfaces/msg/joint_telemetry.hpp"
+#include "custom_crab_interfaces/msg/robot_telemetry.hpp"
+
 
 class CrabPublisher : public rclcpp::Node {
 public:
     CrabPublisher() : Node("crab_publisher") {
-        publisher_ = this->create_publisher<example_interfaces::msg::Float32MultiArray>("crab_publisher", 10);
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(5), 
+        publisher_ = this->create_publisher<custom_crab_interfaces::msg::RobotTelemetry>("crab_publisher", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(500), 
                 std::bind(&CrabPublisher::publishMessage, this));
         RCLCPP_INFO(this->get_logger(), "Crab Publisher has started");
     }
 
 private:
-    rclcpp::Publisher<example_interfaces::msg::Float32MultiArray>::SharedPtr publisher_;
+    rclcpp::Publisher<custom_crab_interfaces::msg::RobotTelemetry>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     void publishMessage() {
-        auto msg = example_interfaces::msg::Float32MultiArray();
-        msg.data = {getTimestamp(), getRandomFloat()};
-        publisher_->publish(msg);
+        auto message = custom_crab_interfaces::msg::RobotTelemetry();
+        message.timestamp = this->now();  // Set current ROS time as the timestamp
+
+        // Simulated telemetry data for 6 sets of joint telemetry
+        for (int i = 0; i < 6; i++) {
+            custom_crab_interfaces::msg::JointTelemetry joint_data;
+            joint_data.joint_number = i;
+            joint_data.input_encoder_radians = getRandomFloat();
+            joint_data.output_encoder_radians = getRandomFloat();
+            joint_data.input_velocity = getRandomFloat();
+            joint_data.output_velocity = getRandomFloat();
+            joint_data.joint_current = getRandomFloat();
+
+            message.data[i] = joint_data;
+        }
+
+        // Publish the message
+        publisher_->publish(message);
     }
 
     float getTimestamp() {
